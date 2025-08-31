735ff6c6b6dd3e1f35e3f7fe07adc4c414e00e07
abhi-jithb <babhijith58@gmail.com>
2025-08-31 15:38:04 +0530

    directory template added for College Vyapari

--- App.tsx
+++ App.tsx
@@ -1,45 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
+export default function App() {
+  return(
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+      <Text style={styles.text}>Hello, my Bahrain <Text style={styles.textN}>friend!</Text></Text>
+      <Text style={styles.text}>Had your <Text style={styles.textN}>food??</Text></Text>
+      <Text style={styles.text}>What's the plan for <Text style={styles.textN}>today?? </Text></Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF', 
+  },
+  text: {
+    fontSize: 18,
+    margin: 10,
+    color: '#333333',
+  },
+  textN:{
+    color: 'violet',
+    fontSize: 22,
+    fontWeight: 'bold',
   },
 });
-
-export default App;
